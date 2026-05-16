86b4fcc6533a4afff76dce1d29ab344a9cc196d6
BuzzAlvin <64155886+BuzzAlvin@users.noreply.github.com>
2026-05-16 19:59:25 +0100

    Update Hero.jsx
    Dynamic cv update 001

--- src/components/section/Hero/Hero.jsx
+++ src/components/section/Hero/Hero.jsx
@@ -43,7 +43,7 @@ const Hero = () => {
 
           <div className={styles.buttonContainer}>
             <motion.a
-              href="https://drive.google.com/uc?export=download&id=https://docs.google.com/file/d/1-2UBN7mpsftwZIjtt7Rb-VvaI5A89qBI/edit?usp=docslist_api&filetype=msword"
+              href="https://drive.google.com/uc?export=download&id=1-2UBN7mpsftwZIjtt7Rb-VvaI5A89qBI"
               className={styles.downloadButton}
               target="_blank"
               rel="noopener noreferrer"
